7c1f964a0d0ecd457ddb86abe8287232e6bec017
Hydro <jermaine.strand@yahoo.com>
2024-10-05 17:10:53 -0400

    more changes

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -9,7 +9,8 @@ SELECT current_database();
 
 CREATE TABLE department (
 id INTEGER PRIMARY KEY,
-name VARCHAR(1) UNIQUE NOT NULL
+name VARCHAR(500) UNIQUE NOT NULL
+
 );
 
 CREATE TABLE role (
@@ -21,16 +22,16 @@ FOREIGN KEY (department) REFERENCES department
 
 );
 
-CREATE TABLE employee (
-id INTEGER PRIMARY KEY,
-first_name VARCHAR(1) NOT NULL,
-last_name VARCHAR(1) NOT NULL,
-title INT NOT NULL,
-department INT NOT NULL,
-salary DECIMAL(10, 2) NOT NULL,
-manager_id INT,
-FOREIGN KEY (title) REFERENCES role,
-FOREIGN KEY (department) REFERENCES department,
-FOREIGN KEY (manager_id) REFERENCES employee
+-- CREATE TABLE employee (
+-- id INTEGER PRIMARY KEY,
+-- first_name VARCHAR(1) NOT NULL,
+-- last_name VARCHAR(1) NOT NULL,
+-- title INT NOT NULL,
+-- department INT NOT NULL,
+-- salary DECIMAL(10, 2) NOT NULL,
+-- manager_id INT,
+-- FOREIGN KEY (title) REFERENCES role,
+-- FOREIGN KEY (department) REFERENCES department,
+-- FOREIGN KEY (manager_id) REFERENCES employee
 
-);
+-- );
