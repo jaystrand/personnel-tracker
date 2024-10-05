49a86bff973d20301253bb81517fd3ef11d0c240
Hydro <jermaine.strand@yahoo.com>
2024-10-05 14:47:58 -0400

    table changes

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO department (id, name) VALUES (1, 'Engineering'), (2, 'Sales'), (3, '
 
 INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 80000, 1), ('Sales Manager', 60000, 2), ('Accountant', 70000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, NULL), ('Jane', 'Smith', 2, NULL), ('Emily', 'Jones', 3, NULL);
+INSERT INTO employee (first_name, last_name, title, department, salary, manager_id) VALUES ('John', 'Doe',1, NULL), ('Mike', 'Chan', 2, NULL), ('Ashley', 'Rodriguez', 3, NULL), ('Kevin', 'Tupik', 4, NULL), ('Kunal', 'Singh', 5, NULL), ('Malia', 'Brown', 6, NULL), ('Sarah', 'Lourd', 7, NULL), ('Tom', 'Allen', 8, NULL);
