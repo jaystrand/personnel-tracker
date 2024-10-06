b1857e8a53f65062493cd63906b9789ee0a57e65
Hydro <jermaine.strand@yahoo.com>
2024-10-06 15:07:30 -0400

    updated schema.sql

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -4,22 +4,22 @@ INSERT INTO department (id, name) VALUES
     (3, 'Finance'),
     (4, 'Legal');
 
-INSERT INTO role (id, title, department_id, salary) VALUES
-    (1, 'Sales Lead', 1, 100000.00),
-    (2, 'Salesperson', 1, 80000.00),
-    (3, 'Lead Engineer', 2, 150000.00),
-    (4, 'Software Engineer', 2, 120000.00),
-    (5, 'Account Manager', 3, 160000.00),
-    (6, 'Accountant', 3, 125000.00),
-    (7, 'Legal Team Lead', 4, 250000.00),
-    (8, 'Lawyer', 4, 190000.00);
+INSERT INTO role (id, title, department, salary) VALUES
+    (1, 'Sales Lead', 'Sales', 100000.00),
+    (2, 'Salesperson', 'Sales', 80000.00),
+    (3, 'Lead Engineer', 'Engineering', 150000.00),
+    (4, 'Software Engineer', 'Engineering', 120000.00),
+    (5, 'Account Manager', 'Finance', 160000.00),
+    (6, 'Accountant', 'Finance', 125000.00),
+    (7, 'Legal Team Lead', 'Legal', 250000.00),
+    (8, 'Lawyer', 'Legal', 190000.00);
 
-INSERT INTO employee (id, first_name, last_name, role_id, department_id, salary, manager_id) VALUES
-    (1, 'John', 'Doe', 1, 1, 100000.00, NULL),
-    (2, 'Mike', 'Chan', 2, 1, 80000.00, 1),
-    (3, 'Ashley', 'Rodriguez', 3, 2, 150000.00, NULL),
-    (4, 'Kevin', 'Tupik', 4, 2, 120000.00, 3),
-    (5, 'Kunal', 'Singh', 5, 3, 160000.00, NULL),
-    (6, 'Malia', 'Brown', 6, 3, 125000.00, 5),
-    (7, 'Sarah', 'Lourd', 7, 4, 250000.00, NULL),
-    (8, 'Tom', 'Allen', 8, 4, 190000.00, 7);
+-- INSERT INTO employee (id, first_name, last_name, role_description, department_description, salary, manager_description) VALUES
+--     (1, 'John', 'Doe', 'Sales Lead', 'Sales', 100000.00, NULL),
+--     (2, 'Mike', 'Chan', 'Salesperson', 'Sales', 80000.00, 'John Doe'),
+--     (3, 'Ashley', 'Rodriguez', 'Lead Engineer', 'Engineering', 150000.00, NULL),
+--     (4, 'Kevin', 'Tupik', 'Software Engineer', 'Engineering', 120000.00, 'Ashley Rodriguez'),
+--     (5, 'Kunal', 'Singh', 'Account Manager', 'Finance', 160000.00, NULL),
+--     (6, 'Malia', 'Brown', 'Accountant', 'Finance', 125000.00, 'Kunal Singh'),
+--     (7, 'Sarah', 'Lourd', 'Legal Team Lead', 'Legal', 250000.00, NULL),
+--     (8, 'Tom', 'Allen', 'Lawyer', 'Legal', 190000.00, 'Sarah Lourd');
