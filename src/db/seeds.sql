414bf24935b2996e5216ffd4ded7f7e2070af69a
Hydro <jermaine.strand@yahoo.com>
2024-10-05 21:15:39 -0400

    added role and employee

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -5,22 +5,22 @@ INSERT INTO department (id, name) VALUES
   (4, 'Legal');
 
 
-INSERT INTO role (, title, department, salary) VALUES
-  (1, 'Sales Lead', 'Sales', 100000.00),
-  (2, 'Salesperson', 'Sales', 80000.00),
-  (3, 'Lead Engineer', 'Engineering', 150000.00),
-  (4, 'Software Engineer', 'Engineering', 120000.00),
-  (5, 'Account Manager', 'Finance', 160000.00),
-  (6, 'Accountant', 'Finance', 125000.00),
-  (7, 'Legal Team Lead', 'Legal', 250000.00),
-  (8, 'Lawyer', 'Legal', 190000.00);
+INSERT INTO role (id, title, department, salary) VALUES
+  (1, 'Sales Lead', 1, 100000.00),
+  (2, 'Salesperson', 1, 80000.00),
+  (3, 'Lead Engineer', 2, 150000.00),
+  (4, 'Software Engineer', 2, 120000.00),
+  (5, 'Account Manager', 3, 160000.00),
+  (6, 'Accountant', 3, 125000.00),
+  (7, 'Legal Team Lead', 4, 250000.00),
+  (8, 'Lawyer', 4, 190000.00);
 
--- INSERT INTO employee (first_name, last_name, title, department, salary, manager_id) VALUES 
--- ('John', 'Doe', 2, 2, 100000,  NULL),
--- ('Mike', 'Chan', 2, 2, 80000.00, 1),
--- ('Ashley', 'Rodriquez', 3, 1, 150000, NULL),
--- ('Kevin', 'Tupik', 4, 1, 120000.00, 3);
--- ('Kunal', 'Singh', 5, 3, 16000.00, NULL),
--- ('Malia', 'Brown', 6, 3, 125000.00, 5),
--- ('Sarah', 'Lourd', 7, 4, 250000.00, NULL),
--- ('Tom', 'Allen', 8, 4, 190000.00, 7);
+INSERT INTO employee (id, first_name, last_name, title, department, salary, manager_id) VALUES 
+(1, 'John', 'Doe', 1, 1, 100000,  NULL),
+(2, 'Mike', 'Chan', 2, 1, 80000.00, 1),
+(3, 'Ashley', 'Rodriquez', 3, 2, 150000, NULL),
+(4, 'Kevin', 'Tupik', 4, 2, 120000.00, 3),
+(5, 'Kunal', 'Singh', 5, 3, 16000.00, NULL),
+(6, 'Malia', 'Brown', 6, 3, 125000.00, 5),
+(7, 'Sarah', 'Lourd', 7, 4, 250000.00, NULL),
+(8, 'Tom', 'Allen', 8, 4, 190000.00, 7);
